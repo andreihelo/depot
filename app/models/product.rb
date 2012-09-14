e4bdf3eef00cf4463b2252ea48de60621e9d9ebe
Andrei Helo <andreihelo@gmail.com>
2012-09-13 21:41:05 -0600

    Just in case before continue

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
-  validates :title, :uniqueness => true
+  validates :title, :length => { :minimum => 10 }
+  validates :title, :uniqueness => {:message => 'ya ha sido utilizado'}
   validates :title, :description, :image_url, :presence => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
   validates :image_url, :format => {
